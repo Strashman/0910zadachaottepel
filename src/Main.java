baac5bba815177cb6152baf619265ffc2497c493
Strashman <>
2024-10-09 20:01:08 +0300

    mn

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,33 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] readData = new int[]{-20, 30, -40, 50, 10, -10};
+
+        readData();
+        showResult();
 
     }
 
-    public static int[] readData();
+    public static int[] readData() {
 
-    {
-        for (int i = 0; i < readData().length; i++)
+        int[] tempList = new int[] {-20, 30, -40, 50, 10, -10};
+        for (int i = 0; i < tempList.length; i++) {
+        return tempList;
+        }
 
 
+    public static int getMaxLenOfPositives(int[] tempList) {
+    int maxLenght = 0;
+    int count =0;
+    for (int t:tempList){
+        if (t>0){
+            count++;
+        } else {
+            if (count>maxLenght) {
+                maxLenght = count;
+            }
+        }
     }
 
-    public static int getMaxLenOfPositives(int[] massiv) {
-
-    }
 
     public void showResult(int result) {
         return;
