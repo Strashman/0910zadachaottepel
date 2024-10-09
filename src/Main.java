2bb012d860e17e6b0bf911ca1e1ee2a9eb70c168
alkaz07 <>
2024-10-09 18:38:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int [] readData  = new int[] {};
+    }
+    public static int [] readData (); {
+        
+    }
+    public static int getMaxLenOfPositives (int[] massiv) {
+
+    }
+    public void showResult (int result){
+        return;
+    }
+
+
+}
